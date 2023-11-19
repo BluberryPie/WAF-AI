a8b0dd5e3e7af8d3d39c54c0f7500ebe492d84b9
blub3rrypie <blub3rrypie@gmail.com>
2023-11-19 22:09:53 +0900

    add: function to initialize/train tokenizer

--- prepare.py
+++ prepare.py
@@ -2,7 +2,7 @@ import conf.preprocess as cfg
 import logging
 import pandas as pd
 
-from src.preprocess import remove_separators, remove_url_encoding
+from src.preprocess import remove_separators, remove_url_encoding, WAFTokenizer
 
 
 def main():
@@ -11,7 +11,7 @@ def main():
     try:
         train_df = pd.read_csv(cfg.TRAIN_DATA_FILEPATH)
     except Exception as e:
-        logging.error(f"{e}\nFailed to read the training data")
+        logging.error(f"[!] Failed to read the training data\n{e}")
     else:
         logging.info("[*] Successfully read the training data")
         logging.info(f"[*] Number of rows: {train_df.shape[0]}")
@@ -21,11 +21,19 @@ def main():
         train_df["payload"] = train_df["payload"].apply(remove_separators)
         train_df["payload"] = train_df["payload"].apply(remove_url_encoding)
     except Exception as e:
-        logging.error(f"{e}\nFailed to preprocess payloads")
+        logging.error(f"[!] Failed to preprocess payloads\n{e}")
     else:
         logging.info("[*] Successfully preprocessed all payloads")
 
     # 3. Train the BPE tokenizer
+    try:
+        logging.info("[*] Initializing & training the tokenizer")
+        waf_tokenizer = WAFTokenizer(vocab_size=cfg.VOCAB_SIZE)
+        waf_tokenizer.train(iterator=train_df["payload"])
+    except Exception as e:
+        logging.error(f"{e}\nFailed to initialize WAF tokenizer")
+    else:
+        waf_tokenizer_fast = waf_tokenizer.get_fast_tokenizer()
 
     # 4. Train the Word2Vec model
 
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,3 +1,9 @@
+from tokenizers import decoders, processors, Tokenizer
+from tokenizers.models import BPE
+from tokenizers.normalizers import NFD
+from tokenizers.pre_tokenizers import Sequence, Whitespace, ByteLevel
+from tokenizers.trainers import BpeTrainer
+from transformers import PreTrainedTokenizerFast
 from urllib.parse import unquote
 
 
@@ -18,4 +24,35 @@ def remove_url_encoding(payload: str) -> str:
         payload = unquoted_payload
 
     return payload
+
+
+class WAFTokenizer():
+
+    def __init__(self, vocab_size, min_frequency=5):
+        # Initialize tokenizer
+        # self.tokenizer = Tokenizer(BPE(unk_token="<UNK>", pad_token="<PAD>"))
+        self.tokenizer = Tokenizer(BPE(unk_token="<UNK>"))
+        self.tokenizer.pre_tokenizer = Sequence([Whitespace(), ByteLevel()])
+        self.tokenizer.normalizer = NFD()
+        self.tokenizer.decoder = decoders.ByteLevel()
+        self.tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
+        self.tokenizer.enable_padding(pad_id=1, pad_token="<PAD>")
+
+        # Initialize BPE trainer
+        self.trainer = BpeTrainer(
+            vocab_size=vocab_size,
+            min_frequency=min_frequency,
+            special_tokens=["<UNK>", "<PAD>"]
+        )
+
+    def train(self, iterator):
+        self.tokenizer.train_from_iterator(iterator, trainer=self.trainer)
+
+    def get_fast_tokenizer(self):
+        fast_tokenizer = PreTrainedTokenizerFast(
+            tokenizer_object=self.tokenizer,
+            unk_token="<UNK>",
+            pad_token="<PAD>"
+        )
+        return fast_tokenizer
 
